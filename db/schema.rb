c747cfe65c448088281b4faa47de503a456f7632
Kiril Tucker <kiril.tucker@gmail.com>
2022-03-17 11:33:45 +0100

    Models created

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_095752) do
+ActiveRecord::Schema.define(version: 2022_03_17_102009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.text "comments"
+    t.text "comment"
     t.bigint "movie_id", null: false
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
