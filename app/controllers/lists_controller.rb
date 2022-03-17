6c4e88b2a493aa49278e087bf2110f6188400f75
Kiril Tucker <kiril.tucker@gmail.com>
2022-03-17 19:49:38 +0100

    End of the day progress

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,41 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.saves
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = @list.bookmarks
+  end
+
+  # def edit
+
+  # end
+
+  # def update
+
+  # end
+
+  # def destroy
+
+  # end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :image_url)
+  end
+end
